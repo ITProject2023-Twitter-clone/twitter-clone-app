daa6add0dbd46f12f21225ac5a64075eed694297
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-01 15:54:11 +0900

    discord authを実装

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,27 +1,4 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/email";
+import { authOptions } from "~/server/auth";
 
-const prisma = new PrismaClient();
-
-export default NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-});
+export default NextAuth(authOptions);
