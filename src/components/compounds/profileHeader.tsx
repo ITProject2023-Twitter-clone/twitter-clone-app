50b7c2ea7c22b37650eed79641457c2a1e895176
Yukiha-ka <yukihakanai.2@gmail.com>
2023-05-09 09:11:56 +0900

    profileHeader returnButton place  fixed

--- src/components/compounds/profileHeader.tsx
+++ src/components/compounds/profileHeader.tsx
@@ -5,12 +5,14 @@ import TweetCount from "../atoms/tweetCount";
 
 const ProfileHeader = () => {
   return (
-    <div className="fixed h-[40px] w-[598px] bg-white opacity-95 z-30 -top-1">
+    <div className="fixed -top-1 z-30 h-[40px] w-[598px] bg-white opacity-95">
       <div className="flex items-center">
-        <ReturnButton />
+        <div className="mt-1 mx-1">
+          <ReturnButton />
+        </div>
         <UserName userName="userName" area="profile" />
       </div>
-      <div className="bottom-2 left-7 relative ">
+      <div className="relative bottom-2 left-9 ">
         <TweetCount />
       </div>
     </div>
