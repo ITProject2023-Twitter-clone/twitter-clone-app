7286ebd9ec16fe21b98a4bcb9a0ab940c7646ed0
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-27 13:31:28 +0900

    fix getMyProfile

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
 import { protectedProcedure } from "~/server/api/trpc";
 import { createTRPCRouter } from "../trpc";
 
@@ -8,26 +7,34 @@ const prisma = new PrismaClient();
 // ログイン中のユーザーのプロフィール情報を取得
 export const profileRouter = createTRPCRouter({
   getMyProfile: protectedProcedure.query(async ({ ctx }) => {
-    const profile = await prisma.profile.findUnique({
+    const profile = await prisma.profile.findFirst({
       where: {
         userId: ctx.session.user.id,
+      },
+      include: {
+        user: {
+          select: {
+            displayName: true,
+          },
+        },
+        socialAccount: true,
       },
     });
     return profile;
   }),
   // 任意のnameのプロフィール情報をreturnする
-  getProfileByName: protectedProcedure
-    .input(z.object({ name: z.string() }))
-    .query(async ({ input }) => {
-      const profile = await prisma.profile.findFirst({
-        where: {
-          user: {
-            name: input.name,
-          },
-        },
-      });
-      return profile;
-    }),
+  // getProfileByName: protectedProcedure
+  //   .input(z.object({ name: z.string() }))
+  //   .query(async ({ input }) => {
+  //     const profile = await prisma.user.findMany({
+  //       where: { name: input.name },
+  //       select: {
+  //         displayName: true,
+  //         profile: true,
+  //       },
+  //     });
+  //     return profile;
+  //   }),
   // プロフィールを編集する
   /**
    * 入力値
