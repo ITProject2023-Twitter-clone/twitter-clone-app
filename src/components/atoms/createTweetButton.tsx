7244af8a1940519b9d06f8870d84cf09aad825b7
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 02:38:17 +0900

    fix css createTweetButton

--- src/components/atoms/createTweetButton.tsx
+++ src/components/atoms/createTweetButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CreateTweetButton = () => {
-  return <button className="w-64 h-20 border border-primary rounded-full text-xl font-bold duration-300 hover:bg-primary">Tweet</button>;
+  return <button className="btn btn-outline text-xl rounded-full w-64 h-20 capitalize font-bold duration-300 hover:bg-primary">Tweet</button>;
 };
 
 export default CreateTweetButton;
