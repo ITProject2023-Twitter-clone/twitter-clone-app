31f25897c48304fbc8f59c92fa244103829afe73
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 02:37:00 +0900

    fix css sidebarNavList

--- src/components/molcules/sidebarNavList.tsx
+++ src/components/molcules/sidebarNavList.tsx
@@ -3,7 +3,7 @@ import SidebarNav from "../atoms/sidebarNav";
 
 const SidebarNavList = () => {
   return (
-    <div className="space-y-10">
+    <div className="space-y-10 w-64">
       <SidebarNav item="Home" />
       <SidebarNav item="Profile" />
       <SidebarNav item="Archive" />
