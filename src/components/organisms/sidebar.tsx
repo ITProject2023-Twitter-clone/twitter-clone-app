7b3a9efdf48f51dd1f6c336f999b093cbb1aa4dc
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 02:40:03 +0900

    create sidebar

---
+++ src/components/organisms/sidebar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SidebarNavList from "../molcules/sidebarNavList";
+import CreateTweetButton from "../atoms/createTweetButton";
+import SidebarUser from "../molcules/sidebarUser";
+
+const Sidebar = () => {
+  return (
+    <div className="flex h-screen w-72 flex-col justify-between text-center">
+      <div className="mx-auto">
+        <SidebarNavList />
+        <div className="mt-14">
+          <CreateTweetButton />
+        </div>
+      </div>
+      <div className="mx-auto">
+        <SidebarUser />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
