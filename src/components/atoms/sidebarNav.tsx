23ae3ccba257b6d054e99e37d07906d7ff8e83b4
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 00:41:45 +0900

    add css sidebarNav

--- src/components/atoms/sidebarNav.tsx
+++ src/components/atoms/sidebarNav.tsx
@@ -7,17 +7,23 @@ type Props = {
   item: ItemType;
 };
 
+
+const HOME = "/Home.svg";
+const PROFILE = "/Profile.svg";
+const ARCHIVE = "/Archive.svg"
+
 //ダミーのsrcを入れてる。適切なsvgに変更してほしい
 const getSrcClass = (item: ItemType): string => {
+
   switch (item) {
     case "Home": {
-      return "/comment-blank-svgrepo-com.svg";
+      return HOME;
     }
     case "Profile": {
-      return "/retweet-svgrepo-com.svg";
+      return PROFILE;
     }
     case "Archive": {
-      return "/twitter-svgrepo-com.svg";
+      return ARCHIVE;
     }
     default: {
       return "";
@@ -29,9 +35,9 @@ const SidebarNav = (props: Props) => {
   const srcClass = getSrcClass(props.item);
   return (
     //div?Link?button?
-    <div>
+    <div className="flex h-14 w-64 items-center rounded-xl p-5 duration-300 hover:bg-neutral">
       <Image src={`${srcClass}`} alt="sidebarItemLogo" width={39} height={39} />
-      <p>{props.item}</p>
+      <p className="ml-8 text-xl">{props.item}</p>
     </div>
   );
 };
