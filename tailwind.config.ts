c01e9ee391b3540d41088c3d7a337ceeeb49b504
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 00:48:37 +0900

    setting daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,10 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-    colors: {
-      primary: '#1DA1F2',
-      secondary: '#14171A',
-      darkGray: '#657786',
-      lightGray: '#AAB8C2',
-      ExtraLightGray: '#E1E8ED',
-      white: '#FFFFFF',
-    }
+
+  daisyui: {
+    themes: ["night"],
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
 } satisfies Config;
