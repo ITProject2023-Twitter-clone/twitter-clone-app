098e4597f7e1fec594e6d03d89c757f7e6ab0ef0
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 00:44:40 +0900

    create sedebarNavList

---
+++ src/components/molcules/sidebarNavList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SidebarNav from "../atoms/sidebarNav";
+
+const SidebarNavList = () => {
+  return (
+    <div className="space-y-10">
+      <SidebarNav item="Home" />
+      <SidebarNav item="Profile" />
+      <SidebarNav item="Archive" />
+    </div>
+  );
+};
+
+export default SidebarNavList;
