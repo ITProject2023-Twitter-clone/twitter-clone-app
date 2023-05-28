a1c97d093b8884cd2dd8f732495ea79e82324911
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 00:38:20 +0900

    create createTweetButton

--- src/components/atoms/createTweetButton.tsx
+++ src/components/atoms/createTweetButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CreateTweetButton = () => {
-  return <button>tweet</button>;
+  return <button className="w-64 h-20 border border-primary rounded-full text-xl font-bold duration-300 hover:bg-primary">Tweet</button>;
 };
 
 export default CreateTweetButton;
