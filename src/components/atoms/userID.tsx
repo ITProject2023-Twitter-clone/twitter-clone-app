105d0ba10469b1a9a21fcb26643f9dcc97698b66
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 00:43:15 +0900

    add css userID

--- src/components/atoms/userID.tsx
+++ src/components/atoms/userID.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 type Props = {
   userID: string;
 };
 
-const UserID = (props:Props) => {
-  return (
-    <p className="text-base text-lightGray">{props.userID}</p>
-  )
-}
+const UserID = (props: Props) => {
+  return <p className="text-base opacity-50">{props.userID}</p>;
+};
 
 export default UserID;
