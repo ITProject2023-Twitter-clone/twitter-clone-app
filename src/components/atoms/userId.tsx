1b38c9ed3d76421bd369020c288f209988c13c48
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 01:09:08 +0900

    change userID.tsx to userId.tsx

---
+++ src/components/atoms/userId.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  userId: string;
+};
+
+const UserId = (props: Props) => {
+  return <p className="text-base opacity-50">{props.userId}</p>;
+};
+
+export default UserId;
