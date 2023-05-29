0b65a986ed0b86b2d7e5aa77fe32dd0dc3c8fe14
VOWAC <yukihakanai.2@gmail.com>
2023-05-30 01:08:31 +0900

    add tweetSetting

---
+++ src/components/molecules/tweetSetting.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import React from "react";
+import Setting from "../../../public/setting.svg";
+
+const TweetSetting = () => {
+  return <Image src={Setting} width={22} height={22} alt="setting" />;
+};
+
+export default TweetSetting;
