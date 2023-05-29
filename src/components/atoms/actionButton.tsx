45dd9cb4afe3b37bc019da82f59480ec4b7d858a
VOWAC <yukihakanai.2@gmail.com>
2023-05-30 00:35:19 +0900

    fix actionButton

--- src/components/atoms/actionButton.tsx
+++ src/components/atoms/actionButton.tsx
@@ -10,19 +10,24 @@ type Props = {
   count: number;
 };
 
+const COMMENT = "/comment.svg"
+const RETWEET = "/retweet.svg"
+const LIKES = "/likes.svg"
+const SHARE = "/share.svg"
+
 const getActionClass = (action: ActionType): string => {
   switch (action) {
     case "Comment": {
-      return "/comment-blank-svgrepo-com.svg";
+      return COMMENT;
     }
     case "Retweet": {
-      return "/retweet-svgrepo-com.svg";
+      return RETWEET;
     }
     case "Likes": {
-      return "/twitter-svgrepo-com.svg";
+      return LIKES;
     }
     case "Share": {
-      return "/share-box-svgrepo-com.svg";
+      return SHARE;
     }
     default: {
       return "";
@@ -30,41 +35,57 @@ const getActionClass = (action: ActionType): string => {
   }
 };
 
-const togleHTMLDisplay = (page: PageType, action: ActionType, count: number) => {
-  switch (page) {
-    case "TweetDetail": {
-        const actionClass = getActionClass(action);
-        return (
-          <button className="w-[29px] flex">
-            <Image src={`${actionClass}`} alt="tweettButton" width={29} height={29} />
-          </button>
-      )
-    }
-    default: {
-      if(action === "Share") {
-        const actionClass = getActionClass(action);
-        return (
-          <button className="w-[58px] flex">
-            <Image src={`${actionClass}`} alt="tweettButton" width={22} height={22} />
-          </button>
-        )
-      } else {
-        const actionClass = getActionClass(action);
-        return (
-          <button className="w-[58px] flex">
-            <Image src={`${actionClass}`} alt="tweettButton" width={22} height={22} />
-            <p className="ml-2 text-base h-5 bottom-[3px] relative text-lightGray">{count}</p>
-          </button>
-      )
-      }
-    }
-  }
-}
+
+
+// const togleHTMLDisplay = (page: PageType, action: ActionType, count: number) => {
+//   switch (page) {
+//     case "TweetDetail": {
+//         const actionClass = getActionClass(action);
+//         return (
+//           <button className="w-[29px] flex">
+//             <Image src={`${actionClass}`} alt="tweettButton" width={29} height={29} />
+//           </button>
+//       )
+//     }
+//     default: {
+//       if(action === "Share") {
+//         const actionClass = getActionClass(action);
+//         return (
+//           <button className="w-[58px] flex">
+//             <Image src={`${actionClass}`} alt="tweettButton" width={22} height={22} />
+//           </button>
+//         )
+//       } else {
+//         const actionClass = getActionClass(action);
+//         return (
+//           <button className="w-[58px] flex">
+//             <Image src={`${actionClass}`} alt="tweettButton" width={22} height={22} />
+//             <p className="ml-2 text-base h-5 bottom-[3px] relative text-lightGray">{count}</p>
+//           </button>
+//       )
+//       }
+//     }
+//   }
 
 const ActionButton = (props: Props) => {
   const actionClass = getActionClass(props.action);
+  const getCount = (count: number) => {
+    switch (props.action) {
+      case "Share": {
+        return "";
+      }
+      default: {
+        return count;
+      }
+    }
+  };
   return (
-    togleHTMLDisplay(props.page, props.action, props.count)
+    <button className="flex items-center">
+      <Image src={`${actionClass}`} alt="tweetButton" width={17} height={17} />
+      <p className="pl-2">
+        {getCount(props.count)}
+      </p>
+    </button>
   );
 };
 export default ActionButton;
