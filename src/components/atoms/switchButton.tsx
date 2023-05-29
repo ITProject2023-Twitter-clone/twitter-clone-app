8ae14d0a346a8a2974d17b4dec7aba29efbd82d7
VOWAC <yukihakanai.2@gmail.com>
2023-05-30 00:37:18 +0900

    fix switchButton

--- src/components/atoms/switchButton.tsx
+++ src/components/atoms/switchButton.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const SwitchButton = (props: Props) => {
-  return <button>{props.switch}</button>;
+  return <button className="font-bold text-base border w-full h-11">{props.switch}</button>;
 };
 
 export default SwitchButton;
