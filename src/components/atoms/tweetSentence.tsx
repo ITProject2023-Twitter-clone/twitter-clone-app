02003d27b74c0f347ef3d264bf27731bc5b36586
VOWAC <yukihakanai.2@gmail.com>
2023-05-30 00:41:06 +0900

    fix tweetSentence

--- src/components/atoms/tweetSentence.tsx
+++ src/components/atoms/tweetSentence.tsx
@@ -5,7 +5,12 @@ type Props = {
 };
 
 const TweetSentence = (props: Props) => {
-  return <p>{props.sentence}</p>;
+  return (
+    //一旦じっと.max-w-とmin-w-のジット以外のあれなに？
+    <div className="w-[600px] break-words">
+      <p className="">{props.sentence}</p>
+    </div>
+  );
 };
 
 export default TweetSentence;
