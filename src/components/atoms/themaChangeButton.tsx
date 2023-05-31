f79af135c3440b03f3baa2c1d2dd67610d9b7638
moXXcha <chacha.love.sho@icloud.com>
2023-06-01 02:36:16 +0900

    create themaChangeButton

---
+++ src/components/atoms/themaChangeButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+
+
+type ButtonType = "dark" | "light"
+
+type Props = {
+    src: string
+    type: ButtonType
+}
+
+const returnStyle = (type: ButtonType) => {
+    switch (type) {
+        case "dark": {
+            return "bg-primary"
+        }
+        case "light": {
+            return "border border-[#F471B5]" 
+        }
+    }
+}
+const themaChangeButton = (props: Props) => {
+  return (
+    <label className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-md ${returnStyle(props.type)}`}>
+      <input type="radio" name="options" data-title="1" className="hidden" />
+      <Image src={props.src} width={20} height={20} alt="light" />
+    </label>
+  );
+};
+
+export default themaChangeButton;
