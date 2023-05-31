09002c434e985625a93d78c11c784b69d4e1b046
moXXcha <chacha.love.sho@icloud.com>
2023-06-01 03:19:46 +0900

    Delete redundant <div> tag

--- src/components/atoms/searchButton.tsx
+++ src/components/atoms/searchButton.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const SearchButton = () => {
   return (
-    <div>
-      <button className="btn-primar btn-outline btn w-40 rounded-full border-[#B5CDF480] text-base capitalize hover:border-none hover:bg-primary">
-        Search
-      </button>
-    </div>
+    <button className="btn-primar btn-outline btn w-40 rounded-full border-[#B5CDF480] text-base capitalize hover:border-none hover:bg-primary">
+      Search
+    </button>
   );
 };
 
