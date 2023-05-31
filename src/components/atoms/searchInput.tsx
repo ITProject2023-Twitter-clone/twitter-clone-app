8a70cd38d6e9754848d8ea74d442330a362f2029
moXXcha <chacha.love.sho@icloud.com>
2023-05-31 17:43:04 +0900

    create searchInput

---
+++ src/components/atoms/searchInput.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "@/public/search.svg";
+import Image from "next/image";
+
+const SearchInput = () => {
+  return (
+    <div className="input input-bordered input-primary w-full max-w-xs flex rounded-full bg-primary bg-opacity-20">
+        <Image src={Search} width={24} height={24} alt="search" className="" />
+      <input type="text" placeholder="Search..." className="bg-transparent placeholder:text-[#B5CDF480] ml-2 outline-none" />
+    </div>
+  );
+};
+
+export default SearchInput;
