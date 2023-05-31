5042274aa6abb08cee634ed0e5f784192fcbe788
moXXcha <chacha.love.sho@icloud.com>
2023-05-31 17:56:33 +0900

    create searchButton

---
+++ src/components/atoms/searchButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchButton = () => {
+  return (
+    <div>
+      <button className="btn-primar btn-outline btn w-40 rounded-full border-[#B5CDF480] text-base capitalize hover:border-none hover:bg-primary">
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchButton;
