03b9498bcbc5cde873c5af64ce19e9edd04bd8fa
VOWAC <yukihakanai.2@gmail.com>
2023-06-01 16:19:11 +0900

    add uploadMedia

---
+++ src/components/atoms/uploadMedia.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import React from "react";
+
+const UploadMedia = () => {
+  return (
+    <label htmlFor="media" className="flex">
+      <input type="file" accept="image/*" className="hidden" id="media" />
+      <Image
+        src="media.svg"
+        width={24}
+        height={24}
+        alt="media"
+        id="media"
+        className=""
+      />
+      <p className="px-3 opacity-50">Upload Media</p>
+    </label>
+  );
+};
+
+export default UploadMedia;
