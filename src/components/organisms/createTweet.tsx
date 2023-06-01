a0be2c12019c9d586b93e2c656002d5123250a9d
VOWAC <yukihakanai.2@gmail.com>
2023-06-01 16:19:20 +0900

    add createTweet

---
+++ src/components/organisms/createTweet.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Icon from "../atoms/icon";
+import UploadMedia from "../atoms/uploadMedia";
+import CreateTweetButton from "../atoms/createTweetButton";
+
+const CreateTweet = () => {
+  return (
+    <div className=" border px-7 pt-5 rounded-2xl">
+      <div className="flex rounded-xl">
+        <Icon size="base" />
+        <div className="w-full">
+          <textarea
+            className="ml-4 w-full bg-inherit pb-28  pt-3 text-lg outline-none"
+            wrap="soft"
+            placeholder="What's Up?"
+          />
+          <div className="my-3 flex items-center justify-between">
+            <UploadMedia />
+            <CreateTweetButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateTweet;
