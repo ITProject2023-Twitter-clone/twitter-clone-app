4dfbcac5d1b44faf362fae6dfaaaaefa26bedd21
kou12345 <hi.0412.ko.2002@gmail.com>
2023-06-01 15:45:01 +0900

    add followUser

---
+++ src/server/api/routers/follow.ts
@@ -0,0 +1,33 @@
+import prisma from "lib/prisma";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const followRouter = createTRPCRouter({
+  // フォローする
+  followUser: protectedProcedure
+    .input(z.object({ targetUserName: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      // フォロー対象のユーザーを取得
+      const targetUser = await prisma.user.findUnique({
+        where: { name: input.targetUserName },
+      });
+
+      // フォローする
+      const follow = await prisma.follow.create({
+        data: {
+          user: { connect: { id: ctx.session.user.id } },
+          followingUser: { connect: { id: targetUser?.id } },
+        },
+      });
+
+      // フォローしたら、フォロワーテーブルにも追加する
+      const follower = await prisma.follower.create({
+        data: {
+          user: { connect: { id: targetUser?.id } },
+          followerUser: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+
+  // フォロー数を取得する
+});
