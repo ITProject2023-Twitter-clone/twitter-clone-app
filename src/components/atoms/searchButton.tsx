6ca3da0fdaaa64153df83afc52a95c5bca8aec08
moXXcha <chacha.love.sho@icloud.com>
2023-06-04 22:01:58 +0900

    remove unused css styles

--- src/components/atoms/searchButton.tsx
+++ src/components/atoms/searchButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchButton = () => {
   return (
-    <button className="btn-primar btn-outline btn w-40 rounded-full border-[#B5CDF480] text-base capitalize hover:border-none hover:bg-primary">
+    <button className="btn-outline btn w-40 rounded-full border-[#B5CDF480] text-base capitalize hover:border-none hover:bg-primary">
       Search
     </button>
   );
