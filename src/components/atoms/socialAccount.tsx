48bd2161eded395a28e70b632bd764998fd7b2c5
moXXcha <chacha.love.sho@icloud.com>
2023-06-04 23:13:05 +0900

    add css socialAcount

--- src/components/atoms/socialAccount.tsx
+++ src/components/atoms/socialAccount.tsx
@@ -10,19 +10,25 @@ type Props = {
 
 //returnのところsvgの画像のパス。
 //適当に変えといて
+
+const GITHUB_ICON = "/githubIcon.svg";
+const TWITTER_ICON = "twitterIcon.svg";
+const QIITA_ICON = "/qiitaIcon.svg";
+const ZENN_ICON = "zennIcon.svg";
+
 const getSNSClass = (action: SNSType): string => {
   switch (action) {
     case "GitHub": {
-      return "/comment-blank-svgrepo-com.svg";
+      return GITHUB_ICON;
     }
     case "Twitter": {
-      return "/retweet-svgrepo-com.svg";
+      return TWITTER_ICON;
     }
     case "Qiita": {
-      return "/twitter-svgrepo-com.svg";
+      return QIITA_ICON;
     }
     case "Zenn": {
-      return "/share-box-svgrepo-com.svg";
+      return ZENN_ICON;
     }
     default: {
       return "";
@@ -30,13 +36,21 @@ const getSNSClass = (action: SNSType): string => {
   }
 };
 
-
 const SocialAccount = (props: Props) => {
   const snsClass = getSNSClass(props.SNS);
   //hrefにはパス
   return (
-    <Link href="">
-      <Image src={`${snsClass}`} width={36} height={36} alt="SNS" />
+    <Link
+      href=""
+      className="flex h-8 w-8 items-center justify-center rounded-full border border-[#B5CDF480]"
+    >
+      <Image
+        src={`${snsClass}`}
+        width={36}
+        height={36}
+        alt="SNS"
+        className="h-4 w-4"
+      />
     </Link>
   );
 };
