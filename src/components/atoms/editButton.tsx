9faea6be290d6e0eaeb5e9dd874b228858466b17
moXXcha <chacha.love.sho@icloud.com>
2023-06-04 22:09:47 +0900

    create editButton

--- src/components/atoms/editButton.tsx
+++ src/components/atoms/editButton.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const EditButton = () => {
   return (
-    <button>Edit profile</button>
-  )
-}
+    <button className="btn h-10 w-28 whitespace-nowrap rounded-full border-[#B5CDF480] bg-transparent text-base capitalize hover:bg-primary hover:text-base-100">
+      Edit Profile
+    </button>
+  );
+};
 
-export default EditButton
+export default EditButton;
