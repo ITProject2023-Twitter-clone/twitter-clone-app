962652568fb2e2137a3be6af3cd6e1183cf8587d
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 04:40:48 +0900

    add border editButton

--- src/components/atoms/editButton.tsx
+++ src/components/atoms/editButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EditButton = () => {
   return (
-    <button className="btn h-10 w-28 whitespace-nowrap rounded-full bg-transparent text-base capitalize hover:bg-primary hover:text-base-100">
+    <button className="btn h-10 w-28 whitespace-nowrap rounded-full bg-transparent text-base border border-white capitalize hover:bg-primary hover:text-base-100">
       Edit Profile
     </button>
   );
