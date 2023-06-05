c270e8d79ded6da1e2e3b7c01540448239a83b7a
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 03:22:38 +0900

    fix style coverImage

--- src/components/atoms/coverImage.tsx
+++ src/components/atoms/coverImage.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Image from 'next/image';
-import Header from "../../../public/kaf.jpeg";
+import React from "react";
+import Image from "next/image";
+import Header from "@/public/kaf.jpeg";
 
 const CoverImage = () => {
   return (
     //ダミーの画像を入れておく、後で変更する
-    <Image
-    src={Header}
-    alt="headerImage"
-    width={608}
-    height={197}
-    className="relation"
-  />
-  )
-}
+    <div className="absolute w-[608px] h-[197px]">
+      <Image
+        src={Header}
+        alt="headerImage"
+        fill
+        className="object-cover"
+      />
+    </div>
+  );
+};
 
-export default CoverImage
+export default CoverImage;
