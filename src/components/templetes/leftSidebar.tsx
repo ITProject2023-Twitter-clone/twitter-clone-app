4c8d8360d30bde66fe3d54bbd4ab10ac3919e547
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 06:13:40 +0900

    create leftSidebar

--- src/components/templetes/leftSidebar.tsx
+++ src/components/templetes/leftSidebar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import SidebarNavList from "../molecules/sidebarNavList";
+import CallCreateTweetButton from "../atoms/callCreateTweetButton";
+import SidebarUser from "../molecules/sidebarUser";
 
-const leftSidebar = () => {
+const LeftSidebar = () => {
   return (
-    <div>leftSidebar</div>
-  )
-}
+    <div className="h-screen w-full">
+      <div className="ml-auto flex h-full w-4/5 flex-col items-center">
+        <div className="space-y-14">
+          <SidebarNavList />
+          <CallCreateTweetButton />
+        </div>
+        <div className="mt-auto">
+          <SidebarUser />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default leftSidebar
+export default LeftSidebar;
