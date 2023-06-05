1d22354b4b7f60f7d602b87705fadfbb840c9009
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 05:55:20 +0900

    format css tweetActionButtons

--- src/components/molecules/tweetActionButtons.tsx
+++ src/components/molecules/tweetActionButtons.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import TweetActionButton from '../atoms/tweetActionButton'
-
+import React from "react";
+import TweetActionButton from "../atoms/tweetActionButton";
 
 const TweeetActionButtons = () => {
   return (
-    <div className='flex items-center justify-between w-full'>
-      <TweetActionButton pageType='tweet' actionType='comment' count={100} />
-      <TweetActionButton pageType='tweet' actionType='retweet' count={100} />
-      <TweetActionButton pageType='tweet' actionType='like' count={100} />
-      <TweetActionButton pageType='tweet' actionType='share' />
+    <div className="flex w-full items-center justify-between">
+      <TweetActionButton pageType="tweet" actionType="comment" count={100} />
+      <TweetActionButton pageType="tweet" actionType="retweet" count={100} />
+      <TweetActionButton pageType="tweet" actionType="like" count={100} />
+      <TweetActionButton pageType="tweet" actionType="share" />
     </div>
-  )
-}
+  );
+};
 
-export default TweeetActionButtons
+export default TweeetActionButtons;
