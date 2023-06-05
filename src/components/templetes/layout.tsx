fe6e0fa98363d97484fdcf6b8ae1eed70a1958ba
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 07:01:20 +0900

    create layout

--- src/components/templetes/layout.tsx
+++ src/components/templetes/layout.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import LeftSidebar from "./leftSidebar";
+import Header from "../organisms/header";
+import RightSidebar from "./rightSidebar";
 
-const layout = () => {
+const Layout = () => {
   return (
-    <div>layout</div>
-  )
-}
+    <div className="flex h-screen w-screen">
+      <div className="h-full w-3/12">
+        <LeftSidebar />
+      </div>
+      <div className="h-full w-5/12 border-l border-r">
+        <Header />
+      </div>
+      <div className="h-full w-4/12">
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
 
-export default layout
+export default Layout;
