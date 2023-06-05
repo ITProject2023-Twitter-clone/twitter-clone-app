d7eccf8dcde8be1d9b8981e650a07482a1aadf64
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 05:44:06 +0900

    fix style tweet

--- src/components/organisms/tweet.tsx
+++ src/components/organisms/tweet.tsx
@@ -3,19 +3,32 @@ import TweetInfo from "../molecules/tweetInfo";
 import Icon from "../atoms/icon";
 import TweetSentence from "../atoms/tweetSentence";
 import TweetSetting from "../molecules/tweetSetting";
-import ActionButtons from "../molecules/actionButtons";
+import ActionButtons from "../molecules/tweetActionButtons";
 
 const Tweet = () => {
   return (
-    <div className="flex w-fit rounded-xl p-3 px-10 bg-base-300">
-      <Icon size="base" />
-      <div className="ml-4 space-y-1">
-        <div className="flex justify-between">
+    <div className="flex w-full space-y-1 rounded-xl bg-base-300 p-4 ">
+      <div className="absolute">
+        <Icon size="base" />
+      </div>
+
+      <div className="w-full pl-20">
+        <div className="flex w-full justify-between">
           <TweetInfo />
           <TweetSetting />
         </div>
-        <TweetSentence sentence="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" />
-        <ActionButtons />
+        <div className="w-full break-words pl-3 pr-10 pt-5">
+          <TweetSentence
+            sentence="これはツイートです。
+                      ツイートの内容です。ツイートの内容です。ツイートの内容です。ツイートの内容です。
+                      ツイートをしましょう。
+                      そう、ツイートを。
+                      そう、ツイートを。
+                      そう、ツイートを。" />
+        </div>
+        <div className="mt-8">
+          <ActionButtons />
+        </div>
       </div>
     </div>
   );
