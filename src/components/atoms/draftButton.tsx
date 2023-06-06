0bc1f7832660aa2af480da8c762c21a6925c2e83
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 17:30:12 +0900

    create draftButton

---
+++ src/components/atoms/draftButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const DraftButton = () => {
+  return (
+    <div className="ml-auto mr-14">
+      <Link href="" className="text-primary">
+        Draft
+      </Link>
+    </div>
+  );
+};
+
+export default DraftButton;
