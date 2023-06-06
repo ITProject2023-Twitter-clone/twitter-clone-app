e2a28c5c9a1cba583c0889f2d475cd4d0bb40dc0
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 13:03:17 +0900

    delete comment header

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -4,7 +4,6 @@ import HeaderSwitch from "../molecules/headerSwitch";
 
 const Header = () => {
   return (
-    //CSSあとで帰るかも、divタグが気持ち悪い
     <div className="w-5/12 fixed z-50 bg-neutral bg-opacity-60">
       <div className="flex h-14 items-center border-b pl-12">
         <HeaderTtile title="Home" />
