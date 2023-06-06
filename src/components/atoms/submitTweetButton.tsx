8254a64ffbf8e044632c5b91dbeb20bd5fd89af2
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 18:22:26 +0900

    fix style submitTweetButton

--- src/components/atoms/submitTweetButton.tsx
+++ src/components/atoms/submitTweetButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const SubmitTweetButton = () => {
   return <button className="btn btn-outline text-xl rounded-full w-36 capitalize font-bold duration-300 hover:bg-primary border-primary hover:border-none">Tweet</button>;
+
 };
 
 export default SubmitTweetButton;
