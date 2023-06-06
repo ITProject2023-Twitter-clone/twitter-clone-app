f7cca7da30c205a2f360ce410ebb32b839cb5c41
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 18:24:17 +0900

    fix z-index header

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -4,7 +4,7 @@ import HeaderSwitch from "../molecules/headerSwitch";
 
 const Header = () => {
   return (
-    <div className="w-5/12 fixed z-50 bg-neutral bg-opacity-60">
+    <div className="w-5/12 fixed z-10 bg-neutral bg-opacity-60">
       <div className="flex h-14 items-center border-b pl-12">
         <HeaderTtile title="Home" />
       </div>
