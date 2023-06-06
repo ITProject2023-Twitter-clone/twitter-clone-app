e0f3ec28d6d03742dbb77e696dfec1472fb0404b
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 12:42:35 +0900

    add style layout

--- src/components/templetes/layout.tsx
+++ src/components/templetes/layout.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import LeftSidebar from "./leftSidebar";
 import Header from "../organisms/header";
 import RightSidebar from "./rightSidebar";
 
-const Layout = () => {
+type Props = {
+  children: ReactNode;
+};
+const Layout = ({ children }: Props) => {
   return (
-    <div className="flex h-screen w-screen">
+    <div className="flex h-screen w-screen bg-neutral">
       <div className="h-full w-3/12">
         <LeftSidebar />
       </div>
-      <div className="h-full w-5/12 border-l border-r">
+      <div className="h-full w-5/12 overflow-auto outline">
         <Header />
+        <div className="mt-28">{children}</div>
       </div>
       <div className="h-full w-4/12">
         <RightSidebar />
