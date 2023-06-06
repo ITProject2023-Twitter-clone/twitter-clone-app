c1e36e6f700d90e3bcd420aa5d0ae81daf060002
moXXcha <chacha.love.sho@icloud.com>
2023-06-06 12:40:36 +0900

    fix style header

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -5,7 +5,7 @@ import HeaderSwitch from "../molecules/headerSwitch";
 const Header = () => {
   return (
     //CSSあとで帰るかも、divタグが気持ち悪い
-    <div className="w-full">
+    <div className="w-5/12 fixed z-50 bg-neutral bg-opacity-60">
       <div className="flex h-14 items-center border-b pl-12">
         <HeaderTtile title="Home" />
       </div>
