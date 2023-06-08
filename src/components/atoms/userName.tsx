34e79320ca1c027990363e28abe96436e19511f6
moXXcha <chacha.love.sho@icloud.com>
2023-06-08 20:17:02 +0900

    fix css userName

--- src/components/atoms/userName.tsx
+++ src/components/atoms/userName.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { SizeType } from '../types';
 
 type Props = {
   userName: string;
+  size?: SizeType
 };
 
 const UserName = (props:Props) => {
   return (
-    <p className="font-bold text-base">{props.userName}</p>
+    <p className={`font-bold ${props.size === "large" ? "text-xl" : "text-base"}`}>{props.userName}</p>
   )
 }
 
