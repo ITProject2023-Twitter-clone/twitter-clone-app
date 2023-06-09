4ac1bdbd22889b5f49fc21907eb950e61e33aae6
moXXcha <chacha.love.sho@icloud.com>
2023-06-10 05:20:43 +0900

    delete margin

--- src/components/atoms/tab.tsx
+++ src/components/atoms/tab.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Tab = (props: Props) => {
   return (
-    <div className="mt-48 w-1/4 flex justify-center h-16">
+    <div className="w-1/4 flex justify-center h-16">
       <button className="hover:bg-ExtraLightGray w-full border-b-primary  font-bold transition duration-300 ease-in-out
       hover:after:mx-auto hover:after:block hover:after:h-1 hover:after:w-14 hover:after:rounded-sm hover:after:bg-primary">
         {props.tabName}
