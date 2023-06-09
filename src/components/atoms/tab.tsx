427d87d473354533552f824c2a514e0d2272c23d
moXXcha <chacha.love.sho@icloud.com>
2023-06-10 03:40:37 +0900

    fix css tab

--- src/components/atoms/tab.tsx
+++ src/components/atoms/tab.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Tab = (props:Props) => {
   return (
-    <button className='w-full font-bold  transition duration-300 ease-in-out hover:bg-ExtraLightGray border-b-primary hover:border-b-2'>{props.tabName}</button>
+    <button className='w-fit pb-1 font-bold  transition duration-300 ease-in-out hover:bg-ExtraLightGray border-b-primary hover:border-b-2'>{props.tabName}</button>
   )
 }
 
