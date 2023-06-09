2a1c890b8154f79d3111f06b71358e2a662a6263
moXXcha <chacha.love.sho@icloud.com>
2023-06-10 05:16:46 +0900

    fix css tab

--- src/components/atoms/tab.tsx
+++ src/components/atoms/tab.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-  tabName : string;
-}
+  tabName: string;
+};
 
-const Tab = (props:Props) => {
+const Tab = (props: Props) => {
   return (
-    <button className='w-fit pb-1 font-bold  transition duration-300 ease-in-out hover:bg-ExtraLightGray border-b-primary hover:border-b-2'>{props.tabName}</button>
-  )
-}
+    <div className="mt-48 w-1/4 flex justify-center h-16">
+      <button className="hover:bg-ExtraLightGray w-full border-b-primary  font-bold transition duration-300 ease-in-out
+      hover:after:mx-auto hover:after:block hover:after:h-1 hover:after:w-14 hover:after:rounded-sm hover:after:bg-primary">
+        {props.tabName}
+      </button>
+    </div>
+  );
+};
 
-export default Tab
+export default Tab;
