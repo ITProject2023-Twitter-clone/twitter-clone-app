23cd369cbc3b42501713efb8c1c325841ba389f0
moXXcha <chacha.love.sho@icloud.com>
2023-06-10 18:42:25 +0900

    fix html headerSwitch

--- src/components/molecules/headerSwitch.tsx
+++ src/components/molecules/headerSwitch.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SwitchElement from "../atoms/switchElement";
 
-const Switch = () => {
+type Page = "followRelation"
+type Props = {
+  page?: Page
+}
+const Switch = (props: Props) => {
   return (
     <div className="flex border-b w-full">
       <div className="w-full border-r">
-        <SwitchElement pageName="Everyone" />
+        <SwitchElement pageName={props.page === "followRelation" ? "followers" :"home"} />
       </div>
       <SwitchElement pageName="Following" />
     </div>
