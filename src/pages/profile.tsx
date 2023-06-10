7698a5f1ba825cae055f237c4b28bae72d8227ea
moXXcha <chacha.love.sho@icloud.com>
2023-06-10 19:31:33 +0900

    add noItem profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import NoItem from "~/components/atoms/noItem";
 import ProfileTopTabList from "~/components/molecules/profileTopTabList";
 import ProfileTop from "~/components/organisms/profileTop";
 import Tweet from "~/components/organisms/tweet";
 
 const Profile = () => {
+  const tweets = [...Array(0)]
   return (
-    <div>
-      <div className="mt-14">
+    <div className="h-full">
+      <div className="pt-14 h-fit">
         <ProfileTop />
         <ProfileTopTabList />
       </div>
-      <div className="flex flex-col items-center space-y-3">
-        {[...Array(10)].map((i: number) => (
+      <div className="flex flex-col items-center space-y-3 h-full">
+        {tweets.length === 0 ?<div className="my-auto"><NoItem item="tweet" /></div> : ""}
+        {tweets.map((i: number) => (
           <div key={i}>
             <Tweet />
           </div>
