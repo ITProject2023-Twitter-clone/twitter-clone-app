f0239a2badd98f6e376db36339ea3d87c271d226
moXXcha <chacha.love.sho@icloud.com>
2023-06-11 18:35:00 +0900

    add css signinButton

--- src/components/atoms/signinButton.tsx
+++ src/components/atoms/signinButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SigninButton = () => {
-  return <button>Sign in with GitHub !!!!!</button>;
+  return <button className="w-96 h-20 border rounded-full bg-transparent hover:bg-primary hover:text-base-200 font-bold hover:border-none">Sign in with GitHub !!!!!</button>;
 };
 
 export default SigninButton;
