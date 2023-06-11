4c2079e01ff6abf2f37304f139647dbde6af83da
moXXcha <chacha.love.sho@icloud.com>
2023-06-11 18:33:51 +0900

    add css signinLogo

--- src/components/atoms/signinLogo.tsx
+++ src/components/atoms/signinLogo.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import github from '@/public/github.svg'
 
 //srcとaltとwidthとheightの指定
 const SigninLogo = () => {
   return (
-    <Image src="" width={0} height={0} alt=''/>
+    <Image src={github} width={221} height={221} alt=''/>
   )
 }
 
