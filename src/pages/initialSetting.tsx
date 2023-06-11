d9530b8452a2e5b3c78a6d67f74057f3de47e3b6
moXXcha <chacha.love.sho@icloud.com>
2023-06-11 21:08:45 +0900

    create initialSetting page

---
+++ src/pages/initialSetting.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SetProfile from '~/components/organisms/setProfile'
+
+const InitialSetting = () => {
+  return (
+    <div className="mt-20">
+        <SetProfile />
+    </div>
+  )
+}
+
+export default InitialSetting
